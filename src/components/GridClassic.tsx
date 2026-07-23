7051ddfb996ad2fb14b358220e99ed770290cc38
Barsegyan <barsegyan@topsystems.ru>
2026-07-23 17:15:50 +0300

    дерево не работает с пагинацией

--- src/components/GridClassic.tsx
+++ src/components/GridClassic.tsx
@@ -1,9 +1,8 @@
 import { Grid } from '@tflex/uikit'
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import { generateProjects } from '@/__mocks__'
 import { useConfig } from '@/components/hooks/useConfig'
-import type { ProjectItemSimple } from "@/models";
 
 export const DataGridClassic = () => {
   const [data, setData] = useState(generateProjects(100, 1))
@@ -18,7 +17,7 @@ export const DataGridClassic = () => {
       pageSize={100}
       totalCount={1000}
       scrollPagination={true}
-      onPageChange={async (page, callbacks) => {
+      onPageChange={async (page) => {
         const objects = generateProjects(100, page)
         console.log('onPageChange',objects, page);
         setData((prev) => [...prev, ...objects])
