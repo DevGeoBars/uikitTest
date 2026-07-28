cf7c48013f43b22cfe2183d3c6acb151a9335998
Barsegyan <barsegyan@topsystems.ru>
2026-07-28 12:23:56 +0300

    -добавить простой TreeView

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import './App.css'
-
-
 import { useRef, useState } from 'react'
-
 
 import {
   DataGrid,
   DataGridClassic,
   TreeList,
-  TreeListClassic,
+  TreeListClassic, TreeViewTest,
 } from '@/components'
-import { useConfig } from "@/components/hooks/useConfig";
-import { generateProjects } from "@/__mocks__";
 
+import './App.css'
+
+type TSelect =
+  | 'gridWithOnNeedLoad'
+  | 'treeWithOnNeedLoad'
+  | 'gridClassic'
+  | 'treeListClassic'
+  | 'treeView'
 
 function App() {
   const componentRef = useRef<any>(null)
 
-  const [type, setType] = useState<
-    | 'gridWithOnNeedLoad'
-    | 'treeWithOnNeedLoad'
-    | 'gridClassic'
-    | 'treeListClassic'
-  >('gridWithOnNeedLoad')
+  const [type, setType] = useState<TSelect>('gridWithOnNeedLoad')
 
   return (
     <div className={'example'}>
@@ -35,6 +32,8 @@ function App() {
           <TreeList />
         ) : type === 'treeListClassic' ? (
           <TreeListClassic />
+        ) : type === 'treeView' ? (
+          <TreeViewTest id={'1'}/>
         ) : (
           <>as</>
         )}
@@ -44,28 +43,16 @@ function App() {
           <span className="mode-info">Тип компоненты</span>
           <select
             value={type}
-            onChange={(e) => setType(e.target.value as any)}
+            onChange={(e) => setType(e.target.value as TSelect)}
             className="mode-select">
             <option value="gridWithOnNeedLoad">Таблица с догрузкой</option>
             <option value="gridClassic">Классическая таблица</option>
             <option value="treeWithOnNeedLoad">Дерево с догрузкой</option>
             <option value="treeListClassic">Дерево классическое</option>
-            <option value="tree">TreeView</option>
+            <option value="treeView">TreeView</option>
           </select>
         </div>
-        {/*<div className={'select'}>*/}
-        {/*  <span className="mode-info">*/}
-        {/*    конфигурация данных (плоская / иерархическая)*/}
-        {/*  </span>*/}
-        {/*  <select*/}
-        {/*    value={columnsConfig}*/}
-        {/*    onChange={(e) => setColumnsConfig(e.target.value as any)}*/}
-        {/*    className="mode-select">*/}
-        {/*    <option value="gridWithOnNeedLoad">Простая таблица</option>*/}
-        {/*    <option value="template">С шаблонными ячейками</option>*/}
-        {/*    <option value="grouped">С группировкой колонок</option>*/}
-        {/*  </select>*/}
-        {/*</div>*/}
+
 
         <button
           onClick={() => console.log('componentRef', componentRef.current)}>
--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -1,12 +1,38 @@
+import { type FC, useState } from 'react'
 import { TreeView} from '@tflex/uikit'
-import { type FC } from 'react'
 
-type TreeViewProps = {}
+import { generateChildren, generateRootTreeData } from "@/__mocks__";
+import FOLDER_LOGO from "@/assets/folderSSO.svg";
+
+type TreeViewProps = {
+  id: string
+}
 
 export const TreeViewTest: FC<TreeViewProps> = () => {
+  const [data, setData] = useState(generateRootTreeData())
+  const [value, setValue] = useState('4')
   return (
     <div className="TreeView-container">
-      <TreeView/>
+      <TreeView
+        width={295}
+
+        data={data}
+        config={{
+          parentKey: 'parentId',
+          idKey: 'guid',
+          labelKey: 'name',
+        }}
+        value={value}
+        getItemProps={item => ({
+          onClick: () => setValue(item.guid!),
+
+          elements: [{ type: 'icon-text', content: item.name, icon: <img src={FOLDER_LOGO} alt={'logo'}/>}],
+          onExpand: async () => {
+            const children = await generateChildren(item, 20);
+            setData((prev) => [...prev, ...children])
+          },
+        })}
+      />
     </div>
   )
 }
